2ceece7e1c75203462a65f6789f093709799b983
NikoDimitrov1991 <ndnikolaydimitrov@gmail.com>
2022-10-30 21:15:23 +0200

    Reformat: code is formatted

--- src/Demo.java
+++ src/Demo.java
@@ -3,9 +3,9 @@ import java.util.Random;
 public class Demo {
     public static void main(String[] args) {
         Random rand = new Random();
-        int [] numbers = new int[10];
+        int[] numbers = new int[10];
 
-        for (int i = 0; i <numbers.length-1 ; i++) {
+        for (int i = 0; i < numbers.length - 1; i++) {
             numbers[i] = rand.nextInt(100);
         }
 
@@ -18,21 +18,22 @@ public class Demo {
         printArray(numbers);
 
     }
-    private static void printArray(int[] numbers){
-        for (int i = 0; i <numbers.length-1 ; i++) {
+
+    private static void printArray(int[] numbers) {
+        for (int i = 0; i < numbers.length - 1; i++) {
             System.out.println(numbers[i]);
         }
     }
 
-    private static  void insertionSort (int[] inputArray){
-        for (int i = 1 ; i <inputArray.length ; i++) {
-        int currentValue = inputArray[i];
-        int j =i-1;
-        while(j>=0 && inputArray[j]>currentValue){
-            inputArray[j+1]=inputArray[j];
-            j--;
-        }
-        inputArray[j+1]=currentValue;
+    private static void insertionSort(int[] inputArray) {
+        for (int i = 1; i < inputArray.length; i++) {
+            int currentValue = inputArray[i];
+            int j = i - 1;
+            while (j >= 0 && inputArray[j] > currentValue) {
+                inputArray[j + 1] = inputArray[j];
+                j--;
+            }
+            inputArray[j + 1] = currentValue;
         }
     }
 }
